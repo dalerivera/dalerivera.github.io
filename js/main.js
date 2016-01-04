3b7be971f26003d177b52c47144997e8ed271e79
Dale Rivera <daleriverah@gmail.com>
2016-01-03 21:08:01 -0500

    Webcam streaming and taking pictures

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//video capture
 'use strict';
 
 navigator.getUserMedia = navigator.getUserMedia ||
@@ -23,3 +24,26 @@ function errorCallback(error) {
 }
 
 navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+
+//take photo
+var canvas = document.querySelector('canvas');
+
+$('button#take_picture').on('click', function(){
+  var c_height = video.videoHeight;
+  var c_width = video.videoWidth;
+  canvas.width = c_width;
+  canvas.height = c_height;
+  var context = canvas.getContext("2d");
+  context.drawImage(video, 0, 0, c_width, c_height);
+  //var dataUrl = canvas.toDataURL("image/jpeg", 0.85);
+  // $.ajax({
+  //   type: "POST",
+  //   script: "/main.py",
+  //   data: {imgBase64: dataUrl},
+  //   success: function(data, status) {
+  //     console.log(data);
+  //   }
+
+  // });
+});
